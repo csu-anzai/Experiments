ab2233f8e11ca728ad14cb62317fd8577bd57c9e
Bomnamul <wdv1235@gmail.com>
2019-08-19 18:13:16 +0900

    Add Hit, Result

---
+++ Car/Assets/Count.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Count : MonoBehaviour
+{
+    Text text;
+
+    private void Awake()
+    {
+        text = GetComponent<Text>();
+    }
+
+    public void Counting(int current, int goal)
+    {
+        text.text = current + " / " + goal;
+    }
+}
